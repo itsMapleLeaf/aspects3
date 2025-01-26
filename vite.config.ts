859871ea401e6c9a273a900a4a358842271bac3e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-01-26 00:44:09 -0600

    netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import mdx from "@mdx-js/rollup"
+import netlify from "@netlify/vite-plugin-react-router"
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-	plugins: [mdx(), reactRouter(), tsconfigPaths(), tailwindcss()],
+	plugins: [
+		mdx(),
+		reactRouter(),
+		tsconfigPaths(),
+		tailwindcss(),
+		process.env.NETLIFY ? netlify() : [],
+	],
 })
