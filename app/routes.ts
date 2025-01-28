c69f792ca966141e56386036d018bb01caabf24c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-01-28 11:42:15 -0600

    Revert "wip image upload"
    This reverts commit d465e47e825673d51f6bed22219c027fdc5c1e3b.

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,4 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes"
 export default [
 	index("routes/rulebook.tsx"),
 	route("character-sheet", "routes/character-sheet/route.tsx"),
-	route("images/upload", "routes/upload-image.ts"),
-	route("images/:id", "routes/get-image.ts"),
 ] satisfies RouteConfig
