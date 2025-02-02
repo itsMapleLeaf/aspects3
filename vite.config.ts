c870e41a4208a6102495a3486410f661c55acb7e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-01 20:42:28 -0600

    add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import netlify from "@netlify/vite-plugin-react-router"
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
+import babel from "vite-plugin-babel"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
 	plugins: [
 		mdx(),
 		reactRouter(),
+		babel({
+			filter: /\.[jt]sx?$/,
+			include: ["app/**"],
+			babelConfig: {
+				presets: ["@babel/preset-typescript"], // if you use TypeScript
+				plugins: ["babel-plugin-react-compiler"],
+			},
+		}),
 		tsconfigPaths(),
 		tailwindcss(),
 		process.env.NETLIFY ? netlify() : [],
