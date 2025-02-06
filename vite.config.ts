025c5b63d5bb1f3c8e3081267b32385e1ffac1a8
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-05 20:31:06 -0600

    add remark gfm plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import mdx from "@mdx-js/rollup"
 import netlify from "@netlify/vite-plugin-react-router"
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
+import remarkGfm from "remark-gfm"
 import { defineConfig } from "vite"
 import babel from "vite-plugin-babel"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
 	plugins: [
-		mdx(),
+		mdx({
+			remarkPlugins: [remarkGfm],
+		}),
 		reactRouter(),
 		tsconfigPaths(),
 		tailwindcss(),
