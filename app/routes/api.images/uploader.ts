6df4ba7303bec49b25e4062e9b80f70c3653bda2
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-09 04:45:08 -0600

    increase max file size

--- app/routes/api.images/uploader.ts
+++ app/routes/api.images/uploader.ts
@@ -9,7 +9,7 @@ export const uploadRouter = {
 			 * For full list of options and defaults, see the File Route API reference
 			 * @see https://docs.uploadthing.com/file-routes#route-config
 			 */
-			maxFileSize: "4MB",
+			maxFileSize: "16MB",
 			maxFileCount: 1,
 		},
 	}).onUploadComplete(async ({ file }) => {
