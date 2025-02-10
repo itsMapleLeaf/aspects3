52dc0cf002c4a3b818d197428fd03f9acb0ccb18
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-09 22:43:01 -0600

    simplify test

--- e2e/character-builder.test.ts
+++ e2e/character-builder.test.ts
@@ -2,75 +2,30 @@ import { expect, test } from "@playwright/test"
 
 test("persistence", async ({ page }) => {
 	const textFields = [
-		{
-			locator: () => page.getByRole("textbox", { name: "Character Name" }),
-			value: "Maple",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Intellect" }),
-			value: "6",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Sense" }),
-			value: "5",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Agility" }),
-			value: "3",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Strength" }),
-			value: "1",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Wit" }),
-			value: "3",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Light" }),
-			value: "2",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Water" }),
-			value: "1",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Wind" }),
-			value: "2",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Fire" }),
-			value: "0",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Darkness" }),
-			value: "1",
-		},
-		{
-			locator: () => page.getByRole("textbox", { name: "Details" }),
-			value: "An intelligent and resourceful character.",
-		},
+		{ name: "Character Name", value: "Maple" },
+		{ name: "Intellect", value: "6" },
+		{ name: "Sense", value: "5" },
+		{ name: "Agility", value: "3" },
+		{ name: "Strength", value: "1" },
+		{ name: "Wit", value: "3" },
+		{ name: "Light", value: "2" },
+		{ name: "Water", value: "1" },
+		{ name: "Wind", value: "2" },
+		{ name: "Fire", value: "0" },
+		{ name: "Darkness", value: "1" },
+		{ name: "Details", value: "An intelligent and resourceful character." },
 	]
 
 	const meterFields = [
-		{
-			name: "Hits",
-			value: "1",
-		},
-		{
-			name: "Fatigue",
-			value: "3",
-		},
-		{
-			name: "Comeback",
-			value: "2",
-		},
+		{ name: "Hits", value: "1" },
+		{ name: "Fatigue", value: "3" },
+		{ name: "Comeback", value: "2" },
 	]
 
 	await page.goto("http://localhost:5173/character-builder")
 
 	for (const field of textFields) {
-		await field.locator().fill(field.value)
+		await page.getByRole("textbox", { name: field.name }).fill(field.value)
 	}
 
 	for (const field of meterFields) {
@@ -82,7 +37,9 @@ test("persistence", async ({ page }) => {
 	await page.reload()
 
 	for (const field of textFields) {
-		await expect(field.locator()).toHaveValue(field.value)
+		await expect(page.getByRole("textbox", { name: field.name })).toHaveValue(
+			field.value,
+		)
 	}
 
 	for (const field of meterFields) {
