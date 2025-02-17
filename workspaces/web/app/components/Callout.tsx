4f553b72cafcad906ebd081e2964fd44a60d5c89
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-17 08:55:43 -0600

    adjust callout style

--- workspaces/web/app/components/Callout.tsx
+++ workspaces/web/app/components/Callout.tsx
@@ -3,7 +3,7 @@ import type { ComponentProps } from "react"
 export function Callout(props: ComponentProps<"aside">) {
 	return (
 		<aside
-			className="border-primary-300 print:border-primary-800 border-l-2 pl-4 *:[p]:my-0"
+			className="border-primary-300 print:border-primary-800 my-6 space-y-3! border-l-2 py-1 pl-4 [&_p]:m-0"
 			{...props}
 		/>
 	)
