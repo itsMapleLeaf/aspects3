c44217e1fcd8a6d78e6ada60e2437a134d04fc10
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-17 07:50:55 -0600

    fix dumb type errors

--- workspaces/web/app/routes/api.images/components.ts
+++ workspaces/web/app/routes/api.images/components.ts
@@ -5,13 +5,25 @@ import {
 } from "@uploadthing/react"
 import type { UploadRouter } from "./uploader.ts"
 
-export const UploadButton = generateUploadButton<UploadRouter>({
-	url: "/api/images",
-})
-export const UploadDropzone = generateUploadDropzone<UploadRouter>({
+// the weird manual type imports fix the portal type issue
+// https://github.com/microsoft/TypeScript/pull/58176
+
+export const UploadButton: ReturnType<
+	typeof generateUploadButton<UploadRouter>
+> = generateUploadButton<UploadRouter>({
 	url: "/api/images",
 })
 
-export const { useUploadThing } = generateReactHelpers<UploadRouter>({
+export const UploadDropzone: ReturnType<
+	typeof generateUploadDropzone<UploadRouter>
+> = generateUploadDropzone<UploadRouter>({
 	url: "/api/images",
 })
+
+const reactHelpers = generateReactHelpers<UploadRouter>({
+	url: "/api/images",
+})
+
+export const useUploadThing: ReturnType<
+	typeof generateReactHelpers<UploadRouter>
+>["useUploadThing"] = reactHelpers.useUploadThing
--- workspaces/web/app/routes/api.images/uploader.ts
+++ workspaces/web/app/routes/api.images/uploader.ts
@@ -1,21 +1,28 @@
-import { createUploadthing, type FileRouter } from "uploadthing/remix"
+import type { Json } from "@uploadthing/shared"
+import { createUploadthing } from "uploadthing/remix"
+import type { FileRoute } from "uploadthing/types"
 
 const uploader = createUploadthing()
 
-export const uploadRouter = {
-	imageUploader: uploader({
-		image: {
-			/**
-			 * For full list of options and defaults, see the File Route API reference
-			 *
-			 * @see https://docs.uploadthing.com/file-routes#route-config
-			 */
-			maxFileSize: "16MB",
-			maxFileCount: 1,
-		},
-	}).onUploadComplete(async ({ file }) => {
-		return {}
-	}),
-} satisfies FileRouter
+const imageUploader: FileRoute<{
+	input: undefined
+	output: {}
+	errorShape: Json
+}> = uploader({
+	image: {
+		/**
+		 * For full list of options and defaults, see the File Route API reference
+		 *
+		 * @see https://docs.uploadthing.com/file-routes#route-config
+		 */
+		maxFileSize: "16MB",
+		maxFileCount: 1,
+	},
+}).onUploadComplete(async ({ file }) => {
+	return {}
+})
 
 export type UploadRouter = typeof uploadRouter
+export const uploadRouter = {
+	imageUploader,
+}
