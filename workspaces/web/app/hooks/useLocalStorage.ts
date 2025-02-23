8a3dd567008b46e90d26608aa5c305a8746250eb
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-22 21:54:03 -0600

    useLocalStorage improvements
    - rename `validate` to `load`
    - more accurate function type
    - use value ref to avoid dependency

--- workspaces/web/app/hooks/useLocalStorage.ts
+++ workspaces/web/app/hooks/useLocalStorage.ts
@@ -1,25 +1,35 @@
 import { useEffect, useLayoutEffect, useState } from "react"
+import { useValueRef } from "./common.ts"
+
+type JsonValue =
+	| string
+	| number
+	| boolean
+	| null
+	| JsonValue[]
+	| { [key: string]: JsonValue }
 
 export function useLocalStorage<T>(
 	key: string,
 	defaultValue: T,
-	validate: (input: unknown) => T,
+	load: (input: JsonValue) => T,
 ) {
 	const [value, setValue] = useState<T>(defaultValue)
 	const [loaded, setLoaded] = useState(false)
+	const loadRef = useValueRef(load)
 
 	useLayoutEffect(() => {
 		if (loaded) return
 		try {
 			const saved = window.localStorage.getItem(key)
 			if (saved) {
-				setValue(validate(JSON.parse(saved)))
+				setValue(loadRef.current(JSON.parse(saved) as JsonValue))
 			}
 			setLoaded(true)
 		} catch (error) {
 			console.warn("failed to parse", error)
 		}
-	}, [key, validate, loaded])
+	}, [loaded, key, loadRef])
 
 	useEffect(() => {
 		if (!loaded) return
