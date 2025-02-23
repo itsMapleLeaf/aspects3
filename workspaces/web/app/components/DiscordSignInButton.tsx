390e947e8641becbcba302bfa668b7e49c4fd1b2
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-22 21:25:29 -0600

    redirect to current path

--- workspaces/web/app/components/DiscordSignInButton.tsx
+++ workspaces/web/app/components/DiscordSignInButton.tsx
@@ -1,13 +1,15 @@
 import { useAuthActions } from "@convex-dev/auth/react"
+import { useLocation } from "react-router"
 import { Button } from "./ui/Button.tsx"
 import { Icon } from "./ui/Icon.tsx"
 
 export function DiscordSignInButton() {
 	const { signIn } = useAuthActions()
+	const location = useLocation()
 	return (
 		<Button
 			icon={<Icon icon="mingcute:discord-fill" />}
-			onClick={() => signIn("discord")}
+			onClick={() => signIn("discord", { redirectTo: location.pathname })}
 		>
 			Sign in with Discord
 		</Button>
