6b5c627f84f00d1ed158a0f9bce686be99df2608
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-26 17:44:43 -0600

    fields are good here

--- workspaces/backend/data/character.ts
+++ workspaces/backend/data/character.ts
@@ -66,7 +66,7 @@ export function parseCharacterFieldsUnsafe(raw: unknown): CharacterFields {
 
 export function parseRemoteCharacterFields(
 	doc: WithoutSystemFields<Doc<"characters">>,
-) {
+): CharacterFields {
 	return parseCharacterFieldsUnsafe(doc.fields ?? doc)
 }
 
