ce6f40f49a8fcbd21446e7b0f5be33f5f0a7343f
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-26 17:39:10 -0600

    clear migration

--- workspaces/backend/convex/migrate.ts
+++ workspaces/backend/convex/migrate.ts
@@ -1,13 +1,3 @@
-import { parseRemoteCharacterFields } from "../data/character.ts"
 import { mutation } from "./_generated/server"
 
-export default mutation(async (ctx, args) => {
-	for await (const character of ctx.db.query("characters")) {
-		const fields = parseRemoteCharacterFields(character)
-		await ctx.db.replace(character._id, {
-			ownerId: character.ownerId,
-			roomId: character.roomId,
-			fields,
-		})
-	}
-})
+export default mutation(async (ctx, args) => {})
