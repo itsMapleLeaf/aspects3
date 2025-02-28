7f780a07d45aa1ef870a662245c9c8d6fcd237f7
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-02-27 20:33:52 -0600

    ok

--- workspaces/web/app/routes/owlbear.manifest[.json]/route.ts
+++ workspaces/web/app/routes/owlbear.manifest[.json]/route.ts
@@ -1,14 +1,22 @@
 export function loader() {
-	return Response.json({
-		name: "Aspects of Nature" + (import.meta.env.DEV ? " [dev]" : ""),
-		version: "0.0.1",
-		manifest_version: 1,
-		action: {
-			title: "Aspects of Nature" + (import.meta.env.DEV ? " [dev]" : ""),
-			icon: "/favicon.svg",
-			popover: "/character-builder",
-			height: 900,
-			width: 500,
+	return Response.json(
+		{
+			name: "Aspects of Nature" + (import.meta.env.DEV ? " [dev]" : ""),
+			version: "0.0.1",
+			manifest_version: 1,
+			action: {
+				title: "Aspects of Nature" + (import.meta.env.DEV ? " [dev]" : ""),
+				icon: "/favicon.svg",
+				popover: "/character-builder",
+				height: 900,
+				width: 500,
+			},
 		},
-	})
+		{
+			headers: {
+				"Access-Control-Allow-Origin": "https://www.owlbear.rodeo",
+				"Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+			},
+		},
+	)
 }
