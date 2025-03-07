12106237b1de88e4e7669939f89fd10b621b078a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-03-07 03:30:11 -0600

    export

--- workspaces/web/app/routes/owlbear.v2.view/CharacterListActions.tsx
+++ workspaces/web/app/routes/owlbear.v2.view/CharacterListActions.tsx
@@ -15,6 +15,19 @@ export function CharacterListActions({
 	onClone,
 	onDelete,
 }: CharacterListActionsProps) {
+	function exportCharacter() {
+		const characterData = JSON.stringify(character, null, 2)
+		const blob = new Blob([characterData], { type: "application/json" })
+		const url = URL.createObjectURL(blob)
+		const a = document.createElement("a")
+		a.href = url
+		a.download = `${character.name.replace(/\s+/g, "_")}.json`
+		document.body.appendChild(a)
+		a.click()
+		document.body.removeChild(a)
+		URL.revokeObjectURL(url)
+	}
+
 	return (
 		<div className="flex gap-1">
 			<SquareIconButton
@@ -29,6 +42,12 @@ export function CharacterListActions({
 				onClick={() => onClone(character)}
 			>
 				Clone
+			</SquareIconButton>
+			<SquareIconButton
+				icon={<Icon icon="mingcute:download-2-fill" className="size-5" />}
+				onClick={exportCharacter}
+			>
+				Export
 			</SquareIconButton>
 			<SquareIconButton
 				icon={<Icon icon="mingcute:edit-2-fill" className="size-5" />}
